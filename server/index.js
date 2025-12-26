6f5a5a6d43226e0fb01959000efdab60a9a06d1f
Rishabh Jain <rishabhsahan@gmail.com>
2025-12-26 21:30:41 +0530

    Update CORS for new Vercel URL

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://127.0.0.1:5173',
-  'https://portfolio-generator-pi-eight.vercel.app',
+  'https://generate-online-portfolio.vercel.app',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
